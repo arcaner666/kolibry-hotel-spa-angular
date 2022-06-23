20c843ad5ba2e921c7757d551d3c04b3a1643430
Arcan Caner Şenol <arcaner666@gmail.com>
2022-06-23 15:37:45 +0300

    updated

--- src/app/components/public/gallery/gallery.component.ts
+++ src/app/components/public/gallery/gallery.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
+import SwiperCore, { SwiperOptions, Navigation} from 'swiper';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-gallery',
@@ -11,13 +14,21 @@ export class GalleryComponent implements OnInit, OnDestroy {
 
   @ViewChild('galleryModal') galleryModal!: ElementRef;
 
+  public config: SwiperOptions = {
+    navigation: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    initialSlide: 0,
+  };
+
   constructor(
     private modalService: NgbModal,
   ) {
     console.log("GalleryComponent constructor çalıştı.");
   }
 
-  openImageModal(): void {
+  openImageModal(initialSlide: number): void {
+    this.config.initialSlide = initialSlide;
     this.modalService.open(this.galleryModal, {
       ariaLabelledBy: 'modal-basic-title',
       centered: true,
