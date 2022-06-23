25c5201e96112be003fb74cdfbd7326afed9300b
Arcan Caner Şenol <arcaner666@gmail.com>
2022-06-23 11:52:43 +0300

    updated

--- src/app/components/public/gallery/gallery.component.ts
+++ src/app/components/public/gallery/gallery.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  @ViewChild('galleryModal') galleryModal!: ElementRef;
+
+  constructor(
+    private modalService: NgbModal,
+  ) {
     console.log("GalleryComponent constructor çalıştı.");
+  }
+
+  openImageModal(): void {
+    this.modalService.open(this.galleryModal, {
+      ariaLabelledBy: 'modal-basic-title',
+      centered: true,
+      
+    });
   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+
+  }
 }
